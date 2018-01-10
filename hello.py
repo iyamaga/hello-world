45ce24145192f20255c26e3ca2c3e16ad5d3252e
iyamaga <iyamaga@yahoo.co.jp>
2018-01-10 12:54:24 +0900

    Update hello.py
    use print options

--- hello.py
+++ hello.py
@@ -3,7 +3,8 @@
 from platform import python_version
 from __future__ import print_function
 
+f = open("hello.txt", "w")
 for i in range(10):
-  print(i, "Hello python", python_version())
-
-# Hello world 3.6.3
+    print(i, "Hello python", python_version(), sep=";")
+    print(i, "Hello python", python_version(), sep=";", end="\n", file=f)
+f.close()

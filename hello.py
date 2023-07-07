10099765a5bdb7c3bb6b47bf1f4cba2087330f07
iyamaga <iyamaga@yahoo.co.jp>
2023-07-07 15:17:13 +0900

    testCommit

--- hello.py
+++ hello.py
@@ -3,8 +3,10 @@
 from __future__ import print_function
 from platform import python_version
 
+## comment
+
 f = open("hello.txt", "w")
 for i in range(10):
-    print(i, "Hello python", python_version(), sep=";")
+    print(i, "Hello pythons3", python_version(), sep=";")
     print(i, "Hello python", python_version(), sep=";", end="\n", file=f)
 f.close()
